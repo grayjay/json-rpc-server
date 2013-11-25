c1ddedc22a5f73173e4ec11831207fe4bd3f09fb
grayjay <grayjay@wordroute.com>
2013-11-24 17:47:52 -0800

    Added response type

--- src/JsonRpcServer.hs
+++ src/JsonRpcServer.hs
@@ -17,6 +17,7 @@ module JsonRpcServer ( RpcResult(..)
                      , liftR) where
 
 import Data.Text hiding (map)
+import Data.String
 import Data.Aeson
 import Data.Aeson.Types
 import Data.Attoparsec.Number
@@ -26,12 +27,17 @@ import Control.Monad
 import Control.Monad.Error
 import Prelude hiding (length)
 
-data RpcError = RpcError Int String Value
+data RpcError = RpcError Int Text (Maybe Value)
               deriving Show
 
 instance Error RpcError where
     noMsg = strMsg "unknown error"
-    strMsg msg = RpcError 32000 msg Null
+    strMsg msg = RpcError 32000 (fromString msg) Nothing
+
+instance ToJSON RpcError where
+    toJSON (RpcError code msg data') = object pairs
+        where pairs = ["code" .= toJSON code, "message" .= toJSON msg] ++ dataPair
+              dataPair = maybe [] (\d -> ["data" .= toJSON d]) data'
 
 type RpcResult m a = ErrorT RpcError m a
 
@@ -66,7 +72,7 @@ method = Method
 
 data Request = Request { rqName :: Text
                        , rqParams :: Either Object Array
-                       , rqId :: (Maybe Id)}
+                       , rqId :: Maybe Id }
 
 instance FromJSON Request where
     parseJSON (Object x) = Request <$>
@@ -83,6 +89,22 @@ instance FromJSON Id where
     parseJSON (Number x) = return $ IdNumber x
     parseJSON Null = return IdNull
 
+instance ToJSON Id where
+    toJSON id = case id of
+                  IdString x -> toJSON x
+                  IdNumber x -> toJSON x
+                  IdNull -> Null
+
+data Response = Response { rspResult :: Either RpcError Value
+                         , rspId :: Id }
+
+instance ToJSON Response where
+    toJSON r = object ["jsonrpc" .= jsonRpcVersion, result, "id" .= (toJSON $ rspId r)]
+        where result = either (("error" .=) . toJSON) ("result" .=) (rspResult r)
+
+jsonRpcVersion :: Text
+jsonRpcVersion = "2.0"
+
 data JsonFunction m = JsonFunction Text (H.HashMap Text Value -> RpcResult m Value)
 
 newtype JsonFunctions m = JsonFunctions (H.HashMap Text (JsonFunction m))
@@ -96,11 +118,15 @@ toJsonFunction' (Method name f params) = JsonFunction name $ mpApply f params
 toJsonFunctions :: [JsonFunction m] -> JsonFunctions m
 toJsonFunctions fs = JsonFunctions $ H.fromList $ map (\f@(JsonFunction n _) -> (n, f)) fs
 
-call :: Monad m => JsonFunctions m -> Value -> m (Either RpcError Value)
-call (JsonFunctions fs) x = runErrorT $ f params
+call :: Monad m => JsonFunctions m -> Value -> m Value
+call (JsonFunctions fs) x = (liftM $ (toJSON . toResponse id)) $ runErrorT $ f params
     where Just (JsonFunction _ f) = H.lookup name fs
           params :: H.HashMap Text Value
           Success (Request name (Left params) id) = fromJSON x
+
+toResponse :: Maybe Id -> Either RpcError Value -> Maybe Response
+toResponse Nothing _ = Nothing
+toResponse (Just id) r = Just $ Response r id
 
 liftR :: (Monad m) => m a -> RpcResult m a
 liftR = lift
