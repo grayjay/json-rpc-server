1d273144cc5e7aa896fdb28c27a7a939caf61926
grayjay <grayjay@wordroute.com>
2013-12-05 14:27:27 -0500

    Improved unit test

--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.JsonRpc.Common
 import Data.JsonRpc.Server
+import Data.Aeson
 import Data.List
 import qualified Data.ByteString.Lazy.Char8 as B
+import Control.Applicative
 import Control.Monad
+import Control.Monad.Identity
 import Test.HUnit
 import Test.Framework
 import Test.Framework.Providers.HUnit
@@ -15,9 +19,13 @@ emptyJsonFunctions :: Monad m => JsonFunctions m
 emptyJsonFunctions = toJsonFunctions []
 
 testInvalidJson :: Assertion
-testInvalidJson = checkCode `liftM` result @? "Failed to detect invalid JSON"
-                  where result :: IO (Maybe B.ByteString)
-                        result = call emptyJsonFunctions "5"
-                        checkCode r = case r of
-                                Nothing -> False
-                                Just json -> "-32700" `isInfixOf` (B.unpack json)
+testInvalidJson = (runIdentity $ getCode `liftM` result) @?= Just (-32700)
+                  where result :: Identity (Maybe Response)
+                        result = (fromByteString =<<) <$> call emptyJsonFunctions "5"
+                        getCode r = case r of
+                                      Just (Response _ (Left (RpcError code _ _))) -> Just code
+                                      _ -> Nothing
+
+fromByteString x = case fromJSON <$> decode x of
+                     Just (Success x') -> Just x'
+                     _ -> Nothing
