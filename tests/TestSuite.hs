6c5d9fb3d3ac1c334b19cf6e2878154590a8b38d
grayjay <grayjay@wordroute.com>
2013-12-04 21:47:42 -0500

    Started adding tests

---
+++ tests/TestSuite.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import JsonRpcServer
+import Data.List
+import qualified Data.ByteString.Lazy.Char8 as B
+import Control.Monad
+import Test.HUnit
+import Test.Framework
+import Test.Framework.Providers.HUnit
+
+main :: IO ()
+main = defaultMain [testCase "invalid JSON" testInvalidJson]
+
+emptyJsonFunctions :: Monad m => JsonFunctions m
+emptyJsonFunctions = toJsonFunctions []
+
+testInvalidJson :: Assertion
+testInvalidJson = checkCode `liftM` result @? "Failed to detect invalid JSON"
+                  where result :: IO (Maybe B.ByteString)
+                        result = call emptyJsonFunctions "5"
+                        checkCode r = case r of
+                                Nothing -> False
+                                Just json -> "-32700" `isInfixOf` (B.unpack json)
