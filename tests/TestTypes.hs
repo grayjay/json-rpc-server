cb4f7cfc77ed91e17b04c15e0355417c8c7f31a9
grayjay <grayjay@wordroute.com>
2013-12-10 21:58:38 -0500

    Added validity checking to parsing of response

--- tests/TestTypes.hs
+++ tests/TestTypes.hs
@@ -4,8 +4,10 @@ module TestTypes ( TestResponse (..)
                  , TestRpcError (..)) where
 
 import Data.Aeson
+import Data.Aeson.Types
 import Data.Text (Text)
 import Data.Attoparsec.Number (Number)
+import Data.HashMap.Strict (size)
 import Control.Applicative
 
 data TestRpcError = TestRpcError { errCode :: Int
@@ -24,10 +26,15 @@ data TestResponse = TestResponse { rspId :: TestId
                                  , rspResult :: Either TestRpcError Value }
 
 instance FromJSON TestResponse where
-    parseJSON (Object r) = TestResponse <$>
+    parseJSON (Object r) = failIf (size r /= 3) *> checkVersion *>
+                           (TestResponse <$>
                            r .: "id" <*>
-                           ((Left <$> r .: "error") <|> (Right <$> r .: "result"))
+                           ((Left <$> r .: "error") <|> (Right <$> r .: "result")))
+        where checkVersion = r .: versionKey >>= failIf . (/= jsonRpcVersion)
     parseJSON _ = empty
+
+failIf :: Bool -> Parser ()
+failIf b = if b then empty else pure ()
 
 data TestId = IdString Text | IdNumber Number | IdNull
 
@@ -42,6 +49,9 @@ instance ToJSON TestId where
                  IdString x -> toJSON x
                  IdNumber x -> toJSON x
                  IdNull -> Null
+
+versionKey :: Text
+versionKey = "jsonrpc"
 
 jsonRpcVersion :: Text
 jsonRpcVersion = "2.0"
