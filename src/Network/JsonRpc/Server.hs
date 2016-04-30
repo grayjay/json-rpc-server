756871a36245bdae5cd8c39c0fd7f74b3b956780
Kristen Kozak <grayjay@wordroute.com>
2016-04-30 15:12:44 -0700

    Updated for GHC 8

--- src/Network/JsonRpc/Server.hs
+++ src/Network/JsonRpc/Server.hs
@@ -75,7 +75,7 @@ import Control.Applicative ((<$>))
 
 -- | Creates a method from a name, function, and parameter descriptions.
 --   The parameter names must be unique.
-toMethod :: (MethodParams f p m r, A.ToJSON r, Monad m) => Text -> f -> p -> Method m
+toMethod :: (MethodParams f p m r) => Text -> f -> p -> Method m
 toMethod name f params = let f' args = A.toJSON `liftM` _apply f params args
                          in Method name f'
 
